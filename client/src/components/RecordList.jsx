a3eb5d5f4b56bb924090014a8c924868f9cd8410
nico-navas-ct <nicolas.navas@ct.uneatlantico.es>
2026-02-18 10:16:59 +0100

    feat: Record.jsx RecordList.jsx and App.jsx deployment

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -43,5 +43,64 @@ export default function RecordList() {
         getRecords();
         return;
     }, [records.length]);
-    
+
+    async function deleteRecord(id) {
+        await fetch(`http://localhost:5050/${id}`, {
+            method: "DELETE"
+        });
+        const newRecords = records.filter((el) => el._id !== id);
+        setRecords(newRecords);
+    }
+
+    function recordList() {
+        return records.map((record) => {
+            return (
+                <Record
+                    record={record}
+                    deleteRecord={() => deleteRecord(record._id)}
+                    key={record._id}
+                />
+            );
+        });
+    }
+
+    return (
+        <>
+            <h3 className="text-lg font-semibold p-4">Employee Records</h3>
+            <div className="border rounded-lg overflow-hidden">
+                <div className="relative w-full overflow-auto">
+                    <table className="w-full caption-bottom text-sm">
+                        <thead className="[&amp;_tr]:border-b">
+                            <tr className="border-b transition-colors hover:bg-muted/50
+                            data-[state=selected]:bg-muted">
+                                <th className="h-12 px-4 text-left align-middle 
+                                font-medium text-muted-foreground [&amp;:has
+                                ([role=checkbox])]:pr-0">
+                                    Name
+                                </th>
+                                <th className="h-12 px-4 text-left align-middle 
+                                font-medium text-muted-foreground [&amp;:has
+                                ([role=checkbox])]:pr-0">
+                                    Position
+                                </th>
+                                <th className="h-12 px-4 text-left align-middle 
+                                font-medium text-muted-foreground [&amp;:has
+                                ([role=checkbox])]:pr-0">
+                                    Level
+                                </th>
+                                <th className="h-12 px-4 text-left align-middle 
+                                font-medium text-muted-foreground [&amp;:has
+                                ([role=checkbox])]:pr-0">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="[&amp;_tr:last-child]:border-0">
+                            {recordList()}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
 }
